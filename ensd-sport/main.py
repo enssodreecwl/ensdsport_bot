d1ba9282aa1f4e240606a6cac631e7447a733e6e
enssodreecwl <mole4156@gmail.com>
2025-12-18 07:58:39 +0500

    fix aiogram 3 executor error

--- ensd-sport/main.py
+++ ensd-sport/main.py
@@ -1,48 +1,23 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
-from aiohttp import web
-from jinja2 import Environment, FileSystemLoader
-import database as db
+import asyncio
+import os
 
-BOT_TOKEN = "8393932502:AAEbOijTuevnjVkhxhtDFUgxNWapBsg3DB4"  # <- вставь сюда токен бота
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
 
-# Проверка, админ ли
-def is_admin(user_id):
-    result = db.cur.execute("SELECT id FROM admins WHERE telegram_id=?", (user_id,)).fetchone()
-    return result is not None
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# /start
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    user_id = message.from_user.id
-    username = message.from_user.username
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
 
-    db.cur.execute("INSERT OR IGNORE INTO users (telegram_id, username) VALUES (?,?)", (user_id, username))
-    db.conn.commit()
+    @dp.message(CommandStart())
+    async def start_handler(message: Message):
+        await message.answer("🤖 Бот запущен и работает!")
 
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton("▶️ Смотреть прогнозы", web_app=types.WebAppInfo(url=f"http://{message.bot.username}.repl.co/")))
-    if is_admin(user_id):
-        keyboard.add(types.InlineKeyboardButton("🛠 Админ-панель", web_app=types.WebAppInfo(url=f"http://{message.bot.username}.repl.co/admin")))
+    await dp.start_polling(bot)
 
-    await message.answer("👋 Добро пожаловать в ENSD SPORT!", reply_markup=keyboard)
 
-# Mini App
-env = Environment(loader=FileSystemLoader('templates'))
-
-async def handle_root(request):
-    template = env.get_template("index.html")
-    return web.Response(text=template.render(), content_type='text/html')
-
-app = web.Application()
-app.router.add_get('/', handle_root)
-
-# Запуск
-if __name__ == "__main__":
-    import asyncio
-    loop = asyncio.get_event_loop()
-    loop.create_task(executor.start_polling(dp, skip_updates=True))
-    web.run_app(app, port=3000)
+if name == "__main__":
+    asyncio.run(main())
